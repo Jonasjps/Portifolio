9c9c86e19b4f73445e477737fce4eba74d6573b4
Jonas Pessoa <jonaspsjw@hotmail.com>
2024-08-17 17:10:50 -0300

    update

---
+++ app.js
@@ -0,0 +1,31 @@
+const typing = document.querySelector('[data-js="typing"]')
+
+const messages = ['Jonas Pessoa']
+
+let messageIndex = 0
+let characterIndex = 0
+let currentMessage = ''
+let currentCharacters = ''
+
+
+const type = () => {
+  const shouldTypeFirstMessage = messageIndex === messages.length
+  
+  if(shouldTypeFirstMessage){
+    messageIndex = 0
+  }
+
+  currentMessage = messages[messageIndex]
+  currentCharacters = currentMessage.slice(0, characterIndex++)
+  typing.textContent = currentCharacters
+
+//   const shouldChangeMessageToBeTyped =
+//     currentCharacters.length === currentMessage.length
+
+//   if( shouldChangeMessageToBeTyped ){
+//     messageIndex++
+//     characterIndex = 0
+//   }
+}
+
+setInterval(type, 200)
